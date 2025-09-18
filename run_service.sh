ac0093fd34d8622b86f0692f7c3dcd3bc30e3974
zhangxuqi <10780027@qq.com>
2025-09-18 22:56:30 +0800

    修改启动脚本

--- run_service.sh
+++ run_service.sh
@@ -1,2 +1,5 @@
-cd /home/tjzs/malio/florence_service
-nohup /home/tjzs/miniconda3/envs/controlnet/bin/python src/florence_api.py --device_id 3 > logs/florence_api.log 2>&1 &
+cd /home/zhangxuqi/zhangxuqi/florence_service
+device_id=0
+port=20093
+python_path=/home/zhangxuqi/miniconda3/envs/malio/bin/python
+nohup $python_path src/florence_api.py --device_id $device_id --port $port 2>&1 |  ts '[%Y-%m-%d %H:%M:%S]' | cronolog logs/${port}_%Y-%m-%d.log &
